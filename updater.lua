de56302811edcb999cbb5359ce6293b8e3968f1f
Umaler <Umaler@Umaler.Umaler>
2025-10-18 14:28:31 +0300

    Add updater

---
+++ updater.lua
@@ -0,0 +1,16 @@
+local shell = require('shell')
+
+local repo = "https://raw.githubusercontent.com/Umaler/WDShipController/refs/heads/"
+local branch = "master"
+
+local function getFileURL(fileName)
+    return string.format(%s%s/%s, repo, branch, fileName)
+end
+
+shell.execute("mkdir /usr")
+shell.execute("mkdir /usr/lib")
+
+shell.execute(string.format('wget -f %s %s', getFileURL("cli.lua"),               "/home/cli.lua"))
+shell.execute(string.format('wget -f %s %s', getFileURL("updater.lua"),           "/home/updater.lua"))
+shell.execute(string.format('wget -f %s %s', getFileURL("WDControllerProxy.lua"), "/usr/lib/WDControllerProxy.lua"))
+shell.execute(string.format('wget -f %s %s', getFileURL("logger.lua"),            "/usr/lib/logger.lua"))
