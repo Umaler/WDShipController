0ec9b0245e753b4459bdef68ac6c0832ab254d55
Umaler <Umaler@Umaler.Umaler>
2025-10-18 22:00:32 +0300

    Fix package caching when updating

--- updater.lua
+++ updater.lua
@@ -7,10 +7,13 @@ local function getFileURL(fileName)
     return string.format("%s%s/%s", repo, branch, fileName)
 end
 
-shell.execute("mkdir /usr")
-shell.execute("mkdir /usr/lib")
+require("filesystem").makeDirectory("/usr/lib")
 
 shell.execute(string.format('wget -f %s %s', getFileURL("cli.lua"),               "/home/cli.lua"))
 shell.execute(string.format('wget -f %s %s', getFileURL("updater.lua"),           "/home/updater.lua"))
 shell.execute(string.format('wget -f %s %s', getFileURL("WDControllerProxy.lua"), "/usr/lib/WDControllerProxy.lua"))
 shell.execute(string.format('wget -f %s %s', getFileURL("logger.lua"),            "/usr/lib/logger.lua"))
+
+local p = require("package")
+p.loaded.WDControllerProxy = nil
+p.logger = nil
